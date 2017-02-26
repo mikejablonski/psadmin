a929a84c1af7280ec931f1d3f8d4a3099a4ab5f7
Mike Jablonski <mjablonski@gmail.com>
2017-02-26 09:43:25 -0800

    routes updates.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes');
 
+// For HTML 5 history URL routing
+// Router.run(routes, Router.HistoryLocation, function(Handler) {
 Router.run(routes, function(Handler) {
     React.render(<Handler/>, document.getElementById('app'));
 });
--- src/routes.js
+++ src/routes.js
@@ -16,6 +16,7 @@ var routes = (
         <NotFoundRoute handler={require('./components/notFoundPage')} />
         <Redirect from="about-us" to="about" />
         <Redirect from="awthurs" to="authors" />
+        <Redirect from="about/*" to="about" />
     </Route>
 );
 
